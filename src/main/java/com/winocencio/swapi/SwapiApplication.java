3bcc97f12da958d643a680bdd7d9e8c029844334
Willian Inocencio <willian.i.alves@hotmail.com>
2022-05-22 05:09:42 -0300

    -FEAT: Startup Log.

--- src/main/java/com/winocencio/swapi/SwapiApplication.java
+++ src/main/java/com/winocencio/swapi/SwapiApplication.java
@@ -1,13 +1,19 @@
 package com.winocencio.swapi;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwapiApplication {
 
+	private static Logger Logger = LoggerFactory.getLogger(SwapiApplication.class);
+	
 	public static void main(String[] args) {
+		Logger.info("Starting the SWAPI api with recommendation.");
 		SpringApplication.run(SwapiApplication.class, args);
+		Logger.info("SWAPI api with recommendation started and ready to receive requests.");
 	}
 
 }
